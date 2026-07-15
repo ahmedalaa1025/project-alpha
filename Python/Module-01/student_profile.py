06b98e14408a5db7d42a820cf0c210a35a5992f8
ahmedalaa1025 <aaganzoury@gmail.com>
2026-07-15 15:06:56 +0300

    Add Python Module-01 programs

---
+++ Python/Module-01/student_profile.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+university = input("Enter your university: ")
+
+major = input("Enter your major: ")
+
+print("========= Student Profile =========")
+
+print(f"Name : {name}")
+print(f"Age : {age}")
+print(f"University : {university}")
+print(f"Major : {major}")
---
+++ Python/Module-01/variables.py
@@ -0,0 +1,27 @@
+"""
+Project Alpha
+Day 7
+Module 01 - Variables
+Author: Ahmed Alaa
+"""
+
+# Store personal information in variables
+
+student_name = "Ahmed Alaa"
+student_age = 20
+student_gpa = 3.30
+is_student = True
+university  = "Arab Academy"
+major = "Computer Engineering"
+
+# Display the information
+
+print("Student Information")
+print("-------------------")
+
+print(f"Name : {student_name}")
+print(f"Age  : {student_age}")
+print(f"GPA  : {student_gpa}")
+print(f"Student : {is_student}")
+print(f"University : {university}")
+print(f"Major : {major}")
